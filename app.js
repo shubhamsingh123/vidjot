1bfbd3966ad0a40ae06059a5fc311d9309c8303c
Shubham Singh <Shubham16cse06@gmail.com>
2021-03-18 23:48:06 +0530

    Setting up the server/port

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require("express");
 
 const app = express();
+
+const port = 3000;
+
+app.listen("/", (req, res) => {
+  console.log(`The server is running in port no ${port}`);
+});

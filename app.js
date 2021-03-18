a2f647c40637c820bcd91eed260a618587f7243c
Shubham Singh <Shubham16cse06@gmail.com>
2021-03-18 23:53:26 +0530

    Basic Routing

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const app = express();
 
+//Index Routing
+app.get("/", (req, res) => {
+  res.send("Index");
+});
+
+// About Routing
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+
 const port = 3000;
 
-app.listen(port, (req, res) => {
+app.listen(port, () => {
   console.log(`The server is running in port no ${port}`);
 });

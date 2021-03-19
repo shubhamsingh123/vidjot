838fbb9099c9253a8ed28fd3903bffbd64f7c491
Shubham Singh <Shubham16cse06@gmail.com>
2021-03-19 19:43:34 +0530

    Creating a Idea Models

---
+++ models/Idea.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// Create Schema
+
+const IdeaSchema = new Schema({
+  title: {
+    type: String,
+    trim: false,
+    required: true,
+  },
+  details: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+mongoose.models("ideas", IdeaSchema);
